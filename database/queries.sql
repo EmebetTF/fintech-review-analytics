639bf511773f2e2871097ec8665b6375a839b949
EmebetTF <279518645+EmebetTF@users.noreply.github.com>
2026-05-22 00:53:52 +0300

    feat: implement PostgreSQL schema and review insertion pipeline

---
+++ database/queries.sql
@@ -0,0 +1,40 @@
+-- Count reviews per bank
+
+SELECT
+    b.bank_name,
+    COUNT(r.review_id) AS total_reviews
+
+FROM reviews r
+
+JOIN banks b
+ON r.bank_id = b.bank_id
+
+GROUP BY b.bank_name;
+
+
+
+-- Average rating per bank
+
+SELECT
+    b.bank_name,
+    AVG(r.rating) AS average_rating
+
+FROM reviews r
+
+JOIN banks b
+ON r.bank_id = b.bank_id
+
+GROUP BY b.bank_name;
+
+
+
+-- Null check
+
+SELECT *
+
+FROM reviews
+
+WHERE
+    review_text IS NULL
+    OR rating IS NULL
+    OR sentiment_label IS NULL;
